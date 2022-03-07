e1c533533c9ccac0d0ac3de459760c326ae117e7
Ariel Tynan <tynan.a@northeastern.edu>
2022-03-07 15:38:25 -0500

    Work in progress trying to use recursion

--- Euler_057_Py.py
+++ Euler_057_Py.py
@@ -2,4 +2,23 @@
 #Euler Problem 057, Square root convergents, solved in Python
 #Started 7 March 2022
 
+from fractions import Fraction
 
+def expansion(num,den,count):
+    while count < 1000:
+        count = count + 1
+        den = den + Fraction(1,2)
+        f = Fraction(num,den)
+        print(Fraction(num,den),count)
+        return expansion(f.numerator, f.denominator,count)
+    return Fraction(num,den) 
+ 
+
+
+print(Fraction(11,35))
+
+frac_list = []
+
+#print(Fraction(1,1) + expansion(1,2,0))
+
+expansion(1,2,0)
