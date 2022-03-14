a1a4cf9867f951739bb9d6f2c20c6feec4eaa935
Ariel Tynan <tynan.a@northeastern.edu>
2022-03-14 12:45:20 -0400

    Solution does not work, cannot deal with very large fractions.

--- Euler_057_Py.py
+++ Euler_057_Py.py
@@ -4,14 +4,23 @@
 
 from fractions import Fraction
 
-def expansion(num,den,count):
+
+
+def expansion(num,count,success):
     while count < 1000:
         count = count + 1
-        den = den + Fraction(1,2)
-        f = Fraction(num,den)
-        print(Fraction(num,den),count)
-        return expansion(f.numerator, f.denominator,count)
-    return Fraction(num,den) 
+        f = Fraction(1 + num).limit_denominator(100000000)
+        print(f,f.numerator,f.denominator,count)
+        str_num = str(f.numerator)
+        str_den = str(f.denominator)
+        if str_num[0] == '1' and str_den[0] != '1':
+            success = success + 1
+            print("YES")
+        num = 1/(2+num)
+        #if f[0] % f[1] > f[0]/2:
+            #success = success + 1
+        return expansion(num,count,success)
+    return success
  
 
 
@@ -21,4 +30,4 @@ frac_list = []
 
 #print(Fraction(1,1) + expansion(1,2,0))
 
-expansion(1,2,0)
+print(expansion(0.5,0,0))
