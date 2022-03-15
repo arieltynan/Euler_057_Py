56efbfff1780efa33bdf2c9582c3ea80e5e855df
Ariel Tynan <tynan.a@northeastern.edu>
2022-03-14 20:30:48 -0400

    One difficult issue was figuring out how to limit the denominator to prevent excess digits that would throw off the fraction and cause it to not reduce. This was fixed by adding in the den_prev value, as consecutive denoms should not be too far from eachother

--- Euler_057_Py.py
+++ Euler_057_Py.py
@@ -6,17 +6,21 @@ from math import floor
 from fractions import Fraction
 from decimal import *
 
-
+#Expansion function uses recursion to layer the fraction shown in the problem
+#Outputs result in fractional form for every added layer
+#Takes in the current total value in "num"
+#Count, the number of iterations
+#Success, the number of successes very len numerator > len denominator
+#den_prev, the previous denominator value for use of limiting the next denominator
 def expansion(num,count,success,den_prev):
-    while count < 1000:
-        count = count + 1
-        getcontext().prec = floor(count)+1000
+    while count < 1000: #1000 iterations
+        count = count + 1 #while loop used for recursion
         f = Fraction(Decimal(1 + num)).limit_denominator(100*(den_prev))
         print(count,f)
         str_num = str(f.numerator)
         str_den = str(f.denominator)
         den_prev = f.denominator
-        if len(str_num) > len(str_den):
+        if len(str_num) > len(str_den): #check num is longer than den each iteration
             success = success + 1
             print("YES")
         num = Decimal(1/(2+num))
@@ -25,13 +29,6 @@ def expansion(num,count,success,den_prev):
     return success
  
 
-
-print(Fraction(11,35))
-
-frac_list = []
-
-#print(Fraction(1,1) + expansion(1,2,0))
-
-getcontext().prec = 300
-print(expansion(0.5,0,0,3))
+getcontext().prec = 1000 #precision to 1000 decimal places
+print(expansion(0.5,0,0,3)) #(num, count, success, den_prev)
 
